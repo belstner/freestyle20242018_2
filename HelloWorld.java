412c3351198fdd81f78cd6ca32f2dbe6d4670c20
belstner <elstner@gmail.com>
2024-02-16 20:55:12 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String [] args) {
-		System.out.println("Hello, god of lightening!");
+		System.out.println("Hello, god of lightening! Polling works");
     }
 }
